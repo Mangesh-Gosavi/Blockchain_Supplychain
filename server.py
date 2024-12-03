bc9ea8a01b54bbc18fb8fbe634e711a0d6c1462e
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-12-03 22:33:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import datetime
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://blockchain-supplychain.onrender.com"}})
+CORS(app, origins="https://blockchain-supplychain.onrender.com")
 
 mongo_uri = os.getenv("MONGO_URI")
 # MongoDB connection
@@ -258,7 +258,6 @@ def viewreport():
 
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=5000)
 
 
